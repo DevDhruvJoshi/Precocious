a2cad0bb73bc9db919a7ef15d7f3955d6df10da9
Dev.Dhruv Joshi <Dev.Dhruvjoshi@gmail.com>
2024-09-17 21:11:47 +0530

    test 2

--- Public/deploy.php
+++ Public/deploy.php
@@ -1,5 +1,7 @@
 <h2>app.dhruvjoshi.dev server</h2>
 <?php
+require_once '../../deploye.php';
+exit;
 
 // Your secret token from GitHub Webhook setup
 $secret = '4cbEmv3UQzs4DUPG8G4zyMhhjpSzQX387whdH0Ciwz8UPDgbE32UgRsjY8kYg7i0'; // Update this if you set a secret
