a966fefdeacd507a4e9935f73d4944ae39fa2432
Dev.Dhruv Joshi <Dev.Dhruvjoshi@gmail.com>
2024-09-24 00:37:20 +0530

    remove code from root index code i wnant to use pu

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
  * vertual host and rewrite is on but not redicting to /public /index.php file so temporary create this, so this is solution is pending
  * 
  */
+/* * /
 require_once ('vendor/autoload.php'); // sencond way to include all files and directory from Oops tuts Videos
 require_once ('System/Init.php');
+/* */ 
